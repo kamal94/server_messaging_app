858ff65c8229679444437becbb0e5eba01bf0074
Kamal Kamalaldin <ask@kamalaldin.com>
2017-03-08 21:07:18 -0500

    Server now simply prints any post requersts

--- desktop_app/app_test.py
+++ desktop_app/app_test.py
@@ -12,15 +12,14 @@ def get_message_info():
 	message_info = {"sender": sender, "receiver": receiver, "date": date, "message": message}
 	return message_info
 
-def post_message(message_info, url_post="http://kamalaldin.com:3000"):
-	#json_message = json.dumps(message_info)
-	req = Request(url_post, urlencode(message_info).encode())
-	post = urlopen(req).read().decode()
+def post_message(url, message_info):
+	json_message = json.dumps(message_info)
+	req = Request(url, urlencode(json_message).encode())
+	post = urlopen(req).read().decode()	#unsure about decode()?
 
-def get_message(url_get="http://kamalaldin.com:3000"):
-	with urlopen(url_get)as response:
-		json_message = response.read().decode("utf-8") 
-	message_info = json.loads(json_message)
+def get_message(url):
+	json_message = urlopen(url).read().decode("utf-8") 
+	message_info = json.load(json_message)
 	return message_info
 
 # Converts json (string) into a python dictionary and displays message info
@@ -28,4 +27,3 @@ def show_messages(message_info):
 	parsed_message = literal_eval(message_info)
 	# The rest of this depends on message format upon retrieval
 
-print(get_message())
