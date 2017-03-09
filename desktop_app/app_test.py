1225ad95eeeeacb1fc4eb2790bcdf86983ea2882
Jeremy Roth <jeremy.c.roth@gmail.com>
2017-03-08 22:34:30 -0500

    Update for desktop app

--- desktop_app/app_test.py
+++ desktop_app/app_test.py
@@ -3,6 +3,7 @@ from urllib.request import Request, urlopen
 from ast import literal_eval
 import json
 import time
+import requests
 
 def get_message_info():
 	sender = input("Please enter your name: ")
@@ -12,14 +13,18 @@ def get_message_info():
 	message_info = {"sender": sender, "receiver": receiver, "date": date, "message": message}
 	return message_info
 
-def post_message(message_info, url_post="http://kamalaldin.com/send:3000"):
-	with urlopen(url_post, urlencode(message_info).encode()) as f:
-		print(f.read().decode('utf-8'))
-	#req = Request(url_post, urlencode(message_info).encode())
-	#post = urlopen(req).read().decode()
+def post_message(message_info, url_post="http://kamalaldin.com:3000/send"):
+	#requests.post(url_post, data = message_info)
+	#request = Request(url_post, urlencode(message_info).encode())
+	#post = urlopen(request).read().decode()
+	string = "?"
+	for key, val in message_info.items():
+		string += key+"="+val+"&"
+	print("requesting", url_post+string)
+	response = requests.post(url_post+string)
 
 def get_messages(url_get="http://kamalaldin.com:3000"):
-	with urlopen(url_get)as response:
+	with urlopen(url_get) as response:
 		json_message = response.read().decode("utf-8") 
 	message_info = json.loads(json_message)
 	return message_info
@@ -27,7 +32,6 @@ def get_messages(url_get="http://kamalaldin.com:3000"):
 def show_messages(message_info_list):
 	for message_info in message_info_list:
 		print(message_info)
-		# Add formatting here
 
 post_message(get_message_info())
 show_messages(get_messages())
