26295ecb1d333a04d234c8b815992ce58c6ad25d
Hans Wieland <Hans@Hanss-MacBook-Air-3.local>
2017-03-09 14:15:28 -0500

    attempted to automate message writing process (input name once and mulitple messages) getting unknown error

--- desktop_app/app_test.py
+++ desktop_app/app_test.py
@@ -5,12 +5,18 @@ import json
 import time
 
 def get_message_info():
+	list= [];
+	y_n = 'y';
 	sender = input("Please enter your name: ")
-	receiver = input("Who would you like to send your message to? ")
-	date = time.strftime("%c")
-	message = input("Write your message: ")
-	message_info = {"sender": sender, "receiver": receiver, "date": date, "message": message}
-	return message_info
+	while y_n == 'y':
+		receiver = input("Who would you like to send your message to? ")
+		date = time.strftime("%c")
+		message = input("Write your message: ")
+		y_n = input("Send another message? (y/n)")
+		message_info = {"sender": sender, "receiver": receiver, "date": date, "message": message}
+		list.append(message_info)
+	print (list)
+	return list
 
 def post_message(message_info, url_post="http://kamalaldin.com/send:3000"):
 	with urlopen(url_post, urlencode(message_info).encode()) as f:
@@ -21,7 +27,7 @@ def post_message(message_info, url_post="http://kamalaldin.com/send:3000"):
 def get_messages(url_get="http://kamalaldin.com:3000"):
 	with urlopen(url_get)as response:
 		json_message = response.read().decode("utf-8") 
-	message_info = json.loads(json_message)
+		message_info = json.loads(json_message)
 	return message_info
 
 def show_messages(message_info_list):
