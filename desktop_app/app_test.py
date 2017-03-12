cdc4ac72949e3d49914bd0270679102e43299e20
Kamal Kamalaldin <ask@kamalaldin.com>
2017-03-12 18:56:58 -0400

    fixed switching chat rooms

--- desktop_app/app_test.py
+++ desktop_app/app_test.py
@@ -8,18 +8,16 @@ import json
 import time
 import os
 
-CHAT_ROOM = "general"
 
 def send_message():
-	global CHAT_ROOM
+	default_chat_room = "general"
 	EXITING = False
 	SWITCHING = True
 	user_name = input("Please enter your user name: ")
 	while not EXITING:
 		chat_room = input("What chat room would you like to enter? (general)")
 		if chat_room == "":
-			chat_room = "general"
-		CHAT_ROOM = chat_room
+			chat_room = default_chat_room
 		SWITCHING = False
 		print("Type 'Exit' to quit the app.\nType 'Switch' to switch chat rooms. ")
 		while not SWITCHING and not EXITING:
@@ -30,9 +28,9 @@ def send_message():
 				elif message == 'Exit':
 					EXITING = True
 				else:
-					message_info = {"user_name": user_name, "chat_room": CHAT_ROOM, "message": message}
+					message_info = {"user_name": user_name, "chat_room": chat_room, "message": message}
 					post_message(message_info)
-			update_messages()
+			update_messages(chat_room)
 
 # Clears the terminal in Mac, Linux, and Windows (I think).
 def clear_terminal():
@@ -42,20 +40,18 @@ def post_message(message_info, url_post="http://kamalaldin.com:3000/send"):
 	requests.post(url_post, json = message_info)
 	print("sending", message_info['message'], "in room", message_info['chat_room'])
 	
-def get_messages(url_get="http://kamalaldin.com:3000", chat_room=CHAT_ROOM, since_minutes=5):
+def get_messages(chat_room, url_get="http://kamalaldin.com:3000", since_minutes=5):
 	print("Receiving messages in chatroom", chat_room)
 	message_info_list = requests.get(url_get, json={"chat_room":chat_room, "since_minutes": since_minutes}).json()
 	return message_info_list
 
 def show_messages(message_info_list):
 	clear_terminal()
-	message_list = get_messages()
-	for message_info in message_list:
-		if message_info ['chat_room'] ==  CHAT_ROOM:
-			print(message_info['chat_room'] + " - " + message_info['user_name'] + ": " + message_info['msg'])
+	for message_info in message_info_list:
+		print(message_info['chat_room'] + " - " + message_info['user_name'] + ": " + message_info['msg'])
 
-def update_messages():
-	show_messages(get_messages())
+def update_messages(chat_room):
+	show_messages(get_messages(chat_room=chat_room))
 	
 if __name__=='__main__':
 	clear_terminal()
