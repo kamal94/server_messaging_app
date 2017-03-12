0c00de7e6546d4f62de1c9faaf4a2c631a18ed0c
Kamal Kamalaldin <ask@kamalaldin.com>
2017-03-12 19:24:15 -0400

    moved app_test.py to app.py

--- desktop_app/app.py
+++ desktop_app/app.py
@@ -1,38 +1,62 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from ast import literal_eval
+from multiprocessing import Process
+from threading import Thread
+import requests
 import json
 import time
-import requests
+import os
+
+CHAT_ROOM = "general"
 
 def send_message():
-	sender = input("Please enter your name: ")
-	while True:
-		chat_room = input("What chat room would you like to enter? ")
-		print("Type 'Exit' to quit the app.\nType 'Switch' to switch chat rooms. ")
-		while True:
-			date = time.strftime("%c")
-			message = input("Write your message: ")
-			message_info = {"user_name": sender, "chat_room": chat_room, "date": date, "message": message}
-			post_message(message_info)
-			if message == 'Switch':
-				break
-			if message == 'Exit':
-				return 1
-		print (list)
-	return 1
+	EXITING = False
+	SWITCHING = True
+	user_name = input("Please enter your user name: ")
+	while not EXITING:
+		global CHAT_ROOM
+		default_chatroom = "general"
+		chat_room = input("What chat room would you like to enter? ({})".format(default_chatroom))
+		# use default chatroom if user didn't specify
+		if chat_room == "":
+			chat_room = default_chatroom
+		CHAT_ROOM = chat_room
+		SWITCHING = False
+		update_messages()
+		while not SWITCHING and not EXITING:
+			message = input(chat_room + " - " + user_name + ": ")
+			if (message.replace(" ","") == '') == False:
+				if message == 'Switch':
+					SWITCHING = True
+				elif message == 'Exit':
+					EXITING = True
+				else:
+					message_info = {"user_name": user_name, "chat_room": CHAT_ROOM, "message": message}
+					post_message(message_info)
+			update_messages()
+
+# Clears the terminal in Mac, Linux, and Windows (I think).
+def clear_terminal():
+	os.system('cls||clear')
 
 def post_message(message_info, url_post="http://kamalaldin.com:3000/send"):
 	requests.post(url_post, json = message_info)
 	
-def get_messages(url_get="http://kamalaldin.com:3000", chat_room="general", since_minutes=5):
-	message_info = requests.get(url_get, json={"chat_room":chat_room, "since_minutes": since_minutes}).json()
-	return message_info
+def get_messages(chat_room, url_get="http://kamalaldin.com:3000", since_minutes=5):
+	message_info_list = requests.get(url_get, json={"chat_room":chat_room, "since_minutes": since_minutes}).json()
+	return message_info_list
 
 def show_messages(message_info_list):
+	clear_terminal()
+	print("Type 'Exit' to quit the app.\nType 'Switch' to switch chat rooms. ")
 	for message_info in message_info_list:
-		print(message_info)
+		print(message_info['chat_room'] + " - " + message_info['user_name'] + ": " + message_info['msg'])
 
-# post_message(get_message_info())
-# show_messages(get_messages())
-print(get_messages())
+def update_messages():
+	global CHAT_ROOM
+	show_messages(get_messages(chat_room=CHAT_ROOM))
+	
+if __name__=='__main__':
+	clear_terminal()
+	p2 = Process(target = send_message()).start()
