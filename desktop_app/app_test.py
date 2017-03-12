ba52d7f5d072777cd3c65a5c7a84aab306acedb4
Kamal Kamalaldin <ask@kamalaldin.com>
2017-03-12 18:21:35 -0400

    Added functionality to display the messages from history and other people in the chatroom

--- desktop_app/app_test.py
+++ desktop_app/app_test.py
@@ -8,12 +8,18 @@ import json
 import time
 import os
 
+CHAT_ROOM = "general"
+
 def send_message():
+	global CHAT_ROOM
 	EXITING = False
 	SWITCHING = True
 	user_name = input("Please enter your user name: ")
 	while not EXITING:
 		chat_room = input("What chat room would you like to enter? ")
+		if chat_room == "":
+			chat_room = CHAT_ROOM
+		CHAT_ROOM = chat_room
 		SWITCHING = False
 		print("Type 'Exit' to quit the app.\nType 'Switch' to switch chat rooms. ")
 		while not SWITCHING and not EXITING:
@@ -23,8 +29,9 @@ def send_message():
 			elif message == 'Exit':
 				EXITING = True
 			else:
-				message_info = {"user_name": user_name, "chat_room": chat_room, "message": message}
+				message_info = {"user_name": user_name, "chat_room": CHAT_ROOM, "message": message}
 				post_message(message_info)
+			update_messages()
 
 	return 1
 
@@ -37,20 +44,21 @@ def post_message(message_info, url_post="http://kamalaldin.com:3000/send"):
 	requests.post(url_post, json = message_info)
 	print("sending", message_info['message'], "in room", message_info['chat_room'])
 	
-def get_messages(url_get="http://kamalaldin.com:3000"):
-	message_info_list = requests.get(url_get).json()
+def get_messages(url_get="http://kamalaldin.com:3000", chat_room=CHAT_ROOM, since_minutes=5):
+	print("Receiving messages in chatroom", chat_room)
+	message_info_list = requests.get(url_get, json={"chat_room":chat_room, "since_minutes": since_minutes}).json()
 	return message_info_list
 
 def show_messages(message_info_list):
 	clear_terminal()
 	message_list = get_messages()
 	for message_info in message_list:
-		print(message_info['user_name'] + ": " + message_info['message'])
+		print(message_info['user_name'] + ": " + message_info['msg'])
 
 def update_messages():
 	show_messages(get_messages())
 	
 if __name__=='__main__':
 	clear_terminal()
-	p1 = Process(target = get_messages()).start()
+	# p1 = Process(target = update_messages()).start()
 	p2 = Process(target = send_message()).start()
